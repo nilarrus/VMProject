2dc931b15e5dceaa93c6100e636ba9434ef3c219
nilarrus <ngarciadelshorts@gmail.com>
2021-01-20 17:23:17 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,18 +16,14 @@
                            
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required  autofocus>
-                                
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $error }}</strong>
+                                </span>
                                 @if( $error = $errors->first('username'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $error }}</strong>
                                     </span>
                                 @endif
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -37,11 +33,6 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
                         <!--  funciones por implementar
