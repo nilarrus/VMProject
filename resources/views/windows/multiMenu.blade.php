826fa0f8574799f318a67ac25cad853c2356504a
nilarrus <ngarciadelshorts@gmail.com>
2021-03-09 17:09:00 +0100

    Update multiMenu.blade.php

--- resources/views/windows/multiMenu.blade.php
+++ resources/views/windows/multiMenu.blade.php
@@ -5,6 +5,37 @@
 <!-- JavaScript -->
 
 <div class="container">
-    Menu Muntiplayer crear sala / unirse a sala  Back
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header bg-dark text-white">Multijugador</div>
+                <div class="card-body bg-secondary text-white">
+                    <div class="container">
+                        <div class="col-md-12 text-center">
+                            <div class="row justify-content-md-center">
+                                <div class="col-md-auto md-5">
+                                    <p class="mb-2">Crear Sala 
+                                        <button class="btn btn-lg btn-primary">+</button>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row justify-content-md-center">
+                                <div class="col-md-auto mb-5">
+                                    <p class="mb-2">Unirse a sala
+                                        <button class="btn btn-lg btn-primary">-</button>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row justify-content-md-rigth">
+                                <div class="col-md-auto mb-5">
+                                    <p class="mt mb-2">Back</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
