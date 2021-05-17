333fd4bcd704fd379192ff268f82613840b8b290
nilarrus <ngarciadelshorts@gmail.com>
2021-05-17 19:47:20 +0200

    Model update

--- app/usersToSala.php
+++ app/usersToSala.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class usersToSala extends Model
+class UsersToSala extends Model
 {
     /**
      * The attributes that are mass assignable.
