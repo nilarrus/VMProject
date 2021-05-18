a62a9b8f65486258f9b9f2d3d7b052cf10dec9d9
nilarrus <ngarciadelshorts@gmail.com>
2021-05-18 21:19:52 +0200

    Update NewRoom.php

--- app/Http/Controllers/NewRoom.php
+++ app/Http/Controllers/NewRoom.php
@@ -18,6 +18,7 @@ class NewRoom extends Controller
 
     public function generarCelesCorrectes($minim)
     {
+        $Celes = new array();
         $Celes = array($minim);
 
         for ($i=0; $i < $minim-1; $i++) { 
