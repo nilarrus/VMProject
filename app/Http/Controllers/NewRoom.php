1f3aa5a8e8dd0684848dc2d078878fc046d3faaa
nilarrus <ngarciadelshorts@gmail.com>
2021-05-21 19:18:09 +0200

    Update NewRoom.php

--- app/Http/Controllers/NewRoom.php
+++ app/Http/Controllers/NewRoom.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Sala;
 use App\UserToSala;
 use App\User;
