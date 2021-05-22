4fa1f6e81f420e0669856a881cc38467afb939dc
nilarrus <ngarciadelshorts@gmail.com>
2021-05-22 03:53:47 +0200

    Update multi.blade.php

--- resources/views/windows/multi.blade.php
+++ resources/views/windows/multi.blade.php
@@ -35,7 +35,7 @@
     </div>
 </div>
 <script> 
-    console.log({{var_dump($JsonCorrectes)}});
+    console.log({{$JsonCorrectes}});
     var level = parseInt($("#nivel").text());
     $("#start").on("click",function(){
         SGame(CCorrectes,level);
