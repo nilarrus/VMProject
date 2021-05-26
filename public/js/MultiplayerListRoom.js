4cd6ac3f2886736c994ae9360766d91667ac224d
nilarrus <ngarciadelshorts@gmail.com>
2021-05-26 21:38:31 +0200

    Update MultiplayerListRoom.js

--- public/js/MultiplayerListRoom.js
+++ public/js/MultiplayerListRoom.js
@@ -1,11 +1,11 @@
-function multiGameList() {
-    console.log("Wola");
+function multiGameList($z) {
+    console.log($z);
 }
 function inGameList() {
     var lista = $("tr#rowList");
     console.log(lista);
     for (let index = 0; index < lista.length; index++) {
-        lista[index].setAttribute("onclick","multiGameList()");
+        lista[index].setAttribute("onclick","multiGameList(this)");
         
     }
 }
