5ce247ef4f69d7f21ffb477ca659adc4119e3339
nilarrus <ngarciadelshorts@gmail.com>
2021-05-26 21:21:43 +0200

    Update multiRoomList.blade.php

--- resources/views/windows/multiRoomList.blade.php
+++ resources/views/windows/multiRoomList.blade.php
@@ -53,6 +53,7 @@
 <script>
     function inGameList() {
         var lista = $("tr.rowList");
+        console.log(lista);
         for (let index = 0; index < lista.length; index++) {
             lista[index].setAttribute("onclick","multiGameList()");
             
