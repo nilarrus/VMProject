fc02cf561c44c9325a4d40b8cff0e35074b4d3a1
nilarrus <ngarciadelshorts@gmail.com>
2021-05-31 17:04:44 +0200

    update

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,14 +1,16 @@
 function checkPassword(tr) {
     var passw = tr.children[0].innerHTML;
+    /* falta pasar el token si es per post
     
     $.ajax({
         type: "POST",
         url: "/mplrch",
-        data: {_token: getCSRFTokenValue() ,pass: passw},
+        data: {_token:  ,pass: passw},
         success: function (response) {
             alert(response);
         }
     });
+    */
 
 }
 
