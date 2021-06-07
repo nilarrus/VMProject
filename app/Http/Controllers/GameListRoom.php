23a80e4f681599f29d79ac88535e8d49989ae7a0
nilarrus <ngarciadelshorts@gmail.com>
2021-06-07 18:14:35 +0200

    Update GameListRoom.php

--- app/Http/Controllers/GameListRoom.php
+++ app/Http/Controllers/GameListRoom.php
@@ -38,10 +38,10 @@ class GameListRoom extends Controller
 
         $pass = $this->checkPasRoom($request);
         
-        $val= Hash::check("1234", $pass);
+        $val= Hash::check("1234", $pass->SPassword);
         var_dump("Valor check hash ", $val);
-        
-        var_dump("Valor de la bbdd " , $pass);
+
+        var_dump("Valor de la bbdd " , $pass->SPassword);
 
         return view('windows.roomPass');
     }
