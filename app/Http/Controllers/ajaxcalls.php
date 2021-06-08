55d43c56bfe0d7bfc4cb3b85188810706ccc8e7b
nilarrus <ngarciadelshorts@gmail.com>
2021-06-08 21:36:04 +0200

    Update ajaxcalls.php

--- app/Http/Controllers/ajaxcalls.php
+++ app/Http/Controllers/ajaxcalls.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Sala;
 
 class ajaxcalls extends Controller
 {
