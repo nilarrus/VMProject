3797dbeda0bcb3a0ea0cd8ee5ef27b3ac464528f
nilarrus <ngarciadelshorts@gmail.com>
2021-06-08 20:07:56 +0200

    Update ajaxTest.blade.php

--- resources/views/ajaxTest.blade.php
+++ resources/views/ajaxTest.blade.php
@@ -13,7 +13,7 @@
             $.ajax({
                 type: 'POST',
                 url: "{{route('tAjax')}}",
-                data: '_token = <?php echo csrf_token() ?> ',
+                data: '_token = <?php echo csrf_token() ?> ','text',
                 success: function (data) {
                     alert(data);
                     $("#msg").html(data.msg);
