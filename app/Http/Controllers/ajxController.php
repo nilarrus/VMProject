e0cf25d9bf4605799ca65f55d7f1fa1ff67e585b
nilarrus <ngarciadelshorts@gmail.com>
2021-06-08 20:20:07 +0200

    Update ajxController.php

--- app/Http/Controllers/ajxController.php
+++ app/Http/Controllers/ajxController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controller\Controller;
+use App\Http\Controllers\Controller;
 
 
 class ajxController extends Controller
