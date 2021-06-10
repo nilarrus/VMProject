2cee8e23165d6a88032d5f8b8b06ff094022291e
nilarrus <ngarciadelshorts@gmail.com>
2021-06-10 19:21:50 +0200

    Update multi.blade.php

--- resources/views/windows/multi.blade.php
+++ resources/views/windows/multi.blade.php
@@ -14,25 +14,46 @@
             <div class="col text-right">Nivel</div>
             <div id="nivel" class="col text-left">@php echo $_POST["level"];@endphp</div> 
         </div>
-        <div class="game centrado">     
-            <table id="table" class="table-game">
-                
-                    <!-- generado por js -->
-                
-            </table>
+        <div class="row mt-0 mb-0">
+            <div class="col">
+                <div class="row">
+                    <div class="col text-right">
+                        <img src="{{URL::asset('img/clock.png')}}" alt="" width="45" height="45">
+                    </div>
+                    <div class="col text-left">
+                        <p id="gameTime">0</p>
+                    </div>
+                </div>           
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col text-right">
+                        <img src="{{URL::asset('img/wrong.png')}}" alt="" width="45" height="45">
+                    </div>
+                    <div class="col text-left">
+                        <p id="gameFails">0</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="backAjax"></div>
-        <button id="start" class="btn btn-primary" type="button" onclick="celesServer('{{$_POST['nsala']}}')" > Call ajax </button>
-
-        @if (Auth::user()->username == $creador)
-            <form action="{{ route('deleteRoom')}}" method="post" id="formReturn" >
-                @csrf
-                <input type="text" name="nsala" id="nsala" value="{{$_POST["nsala"]}}" hidden><br/>
-                <button class="btn btn-dark" type="submit">Borrar sala</button><br/>
-            </form>
-        @endif
-        
     </div>
+    <div class="game centrado">     
+        <table id="table" class="table-game">
+            
+                <!-- generado por js -->
+            
+        </table>
+    </div>
+    <div id="backAjax"></div>
+    <button id="start" class="btn btn-primary" type="button" onclick="celesServer('{{$_POST['nsala']}}')" > Call ajax </button>
+
+    @if (Auth::user()->username == $creador)
+        <form action="{{ route('deleteRoom')}}" method="post" id="formReturn" >
+            @csrf
+            <input type="text" name="nsala" id="nsala" value="{{$_POST["nsala"]}}" hidden><br/>
+            <button class="btn btn-dark" type="submit">Borrar sala</button><br/>
+        </form>
+    @endif
 </div>
 <script> 
     var level = parseInt($("#nivel").text());
